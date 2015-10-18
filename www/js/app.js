b8e0ec78979830b4b9e0af16c7b561fa7d43fd60
Shashank Bharadwaj <shashu10@gmail.com>
2015-10-17 22:35:11 -0700

    Added schedule view

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'flexcalendar' , 'pascalprecht.translate'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -41,11 +41,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
+  .state('app.schedule', {
+      url: '/schedule',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/schedule.html'
         }
       }
     })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -53,4 +53,34 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+})
+
+.controller('ScheduleCtrl', ['$scope', function($scope) {
+    $scope.options = {
+    defaultDate: "2015-08-06",
+    minDate: "2015-01-01",
+    maxDate: "2015-12-31",
+    disabledDates: [
+        "2015-06-22",
+        "2015-07-27",
+        "2015-08-13",
+        "2015-08-15"
+    ],
+    dayNamesLength: 1, // 1 for "M", 2 for "Mo", 3 for "Mon"; 9 will show full day names. Default is 1.
+    mondayIsFirstDay: true,//set monday as first day of week. Default is false
+    eventClick: function(date) {
+      console.log(date);
+    },
+    dateClick: function(date) {
+      console.log(date);
+    },
+    changeMonth: function(month, year) {
+      console.log(month, year);
+    },
+  };
+
+  $scope.events = [
+    {foo: 'bar', date: "2015-08-18"},
+    {foo: 'bar', date: "2015-08-20"}
+  ];
+}]);
