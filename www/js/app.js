1cb6341cd555ad5ed42e8de598d29609ad40c222
Shashank Bharadwaj <shashu10@gmail.com>
2015-11-01 14:37:37 -0800

    added iOS9 patch. Removes UI flicker and other problems

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,10 @@
-// Ionic Starter App
+// Ionic Workgenius App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'workgenius' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'workgenius.controllers', 'workgenius.directives', 'workgenius.filters', 'flexcalendar' , 'pascalprecht.translate', 'ionic-timepicker', 'ionic.ion.autoListDivider'])
+// 'workgenius.controllers' is found in controllers.js
+angular.module('workgenius', ['ionic', 'workgenius.controllers', 'workgenius.directives', 'workgenius.filters', 'flexcalendar' , 'pascalprecht.translate', 'ionic-timepicker', 'ionic.ion.autoListDivider', 'ngIOS9UIWebViewPatch'])
 
 .run(function($ionicPlatform, $rootScope, $state) {
   $ionicPlatform.ready(function() {
