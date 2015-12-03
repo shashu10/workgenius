915a2b0b228304e081146c3cec1dab7e5ffcf33e
Shashank Bharadwaj <shashu10@gmail.com>
2015-12-03 10:55:33 -0800

    show/hide continue button after selecting input

--- www/js/filters.js
+++ www/js/filters.js
@@ -5,7 +5,17 @@ angular.module('workgenius.filters', [])
     return input && (input.substring(0,1).toUpperCase()+input.substring(1));
   };
 })
-
+.filter('isEmpty', function () {
+    var bar;
+    return function (obj) {
+        for (bar in obj) {
+            if (obj.hasOwnProperty(bar)) {
+                return false;
+            }
+        }
+        return true;
+    };
+})
 .filter('truncateObj', [function(){
     return function(obj, limit){
         if (!obj) {
