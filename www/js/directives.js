e864651f2273ec395940f34ac40ab1bfdd35b6a3
Shashank Bharadwaj <shashu10@gmail.com>
2016-03-21 13:32:08 -0700

    removed profile photo

--- www/js/directives.js
+++ www/js/directives.js
@@ -231,119 +231,119 @@ angular.module('workgenius.directives', [])
     });
   };
 }])
-.directive('wgProfilePhoto', function() {
-  return {
-      templateUrl: 'templates/shared/wg-profile-photo.html',
-      controller: ['$scope', '$ionicActionSheet', '$cordovaCamera', '$rootScope', function($scope, $ionicActionSheet, $cordovaCamera, $rootScope) {
+// .directive('wgProfilePhoto', function() {
+//   return {
+//       templateUrl: 'templates/shared/wg-profile-photo.html',
+//       controller: ['$scope', '$ionicActionSheet', '$cordovaCamera', '$rootScope', function($scope, $ionicActionSheet, $cordovaCamera, $rootScope) {
 
-          $scope.changePhoto = function() {
-              // Show the action sheet
-              var hideSheet = $ionicActionSheet.show({
-                  buttons: [{
-                      text: 'Choose from library'
-                  }, {
-                      text: 'Take profile picture'
-                  }],
-                  destructiveText: 'Remove photo',
-                  titleText: 'Change your profile photo',
-                  cancelText: 'Cancel',
-                  cancel: function() {
-                      // add cancel code..
-                  },
-                  destructiveButtonClicked: function () {
-                    $rootScope.imageURL = "img/profile_default.jpg";
-                    hideSheet();
-                  },
-                  buttonClicked: function(index) {
-                      switch(index) {
-                        case 0:
-                          $scope.addImage("PHOTOLIBRARY");
-                          break;
-                        case 1:
-                          $scope.addImage("CAMERA");
-                          break;
-                      }
-                      hideSheet();
-                  }
-              });
-          };
+//           $scope.changePhoto = function() {
+//               // Show the action sheet
+//               var hideSheet = $ionicActionSheet.show({
+//                   buttons: [{
+//                       text: 'Choose from library'
+//                   }, {
+//                       text: 'Take profile picture'
+//                   }],
+//                   destructiveText: 'Remove photo',
+//                   titleText: 'Change your profile photo',
+//                   cancelText: 'Cancel',
+//                   cancel: function() {
+//                       // add cancel code..
+//                   },
+//                   destructiveButtonClicked: function () {
+//                     $rootScope.imageURL = "img/profile_default.jpg";
+//                     hideSheet();
+//                   },
+//                   buttonClicked: function(index) {
+//                       switch(index) {
+//                         case 0:
+//                           $scope.addImage("PHOTOLIBRARY");
+//                           break;
+//                         case 1:
+//                           $scope.addImage("CAMERA");
+//                           break;
+//                       }
+//                       hideSheet();
+//                   }
+//               });
+//           };
 
-          $scope.addImage = function(type) {
-              // 2
-              var options = {
-                  destinationType: Camera.DestinationType.FILE_URI,
-                  sourceType: Camera.PictureSourceType[type],
-                  allowEdit: false,
-                  encodingType: Camera.EncodingType.JPEG,
-                  correctOrientation: true,
-                  popoverOptions: CameraPopoverOptions,
-              };
+//           $scope.addImage = function(type) {
+//               // 2
+//               var options = {
+//                   destinationType: Camera.DestinationType.FILE_URI,
+//                   sourceType: Camera.PictureSourceType[type],
+//                   allowEdit: false,
+//                   encodingType: Camera.EncodingType.JPEG,
+//                   correctOrientation: true,
+//                   popoverOptions: CameraPopoverOptions,
+//               };
 
-              // 3
-              $cordovaCamera.getPicture(options).then(function(imageData) {
+//               // 3
+//               $cordovaCamera.getPicture(options).then(function(imageData) {
 
-                  // 4
-                  onImageSuccess(imageData);
+//                   // 4
+//                   onImageSuccess(imageData);
 
-                  function onImageSuccess(fileURI) {
-                      createFileEntry(fileURI);
-                  }
+//                   function onImageSuccess(fileURI) {
+//                       createFileEntry(fileURI);
+//                   }
 
-                  function createFileEntry(fileURI) {
-                      window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
-                  }
+//                   function createFileEntry(fileURI) {
+//                       window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
+//                   }
 
-                  // 5
-                  function copyFile(fileEntry) {
-                      var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
-                      var newName = makeid() + name;
+//                   // 5
+//                   function copyFile(fileEntry) {
+//                       var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
+//                       var newName = makeid() + name;
 
-                      window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
-                              fileEntry.copyTo(
-                                  fileSystem2,
-                                  newName,
-                                  onCopySuccess,
-                                  fail
-                              );
-                          },
-                          fail);
-                  }
+//                       window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
+//                               fileEntry.copyTo(
+//                                   fileSystem2,
+//                                   newName,
+//                                   onCopySuccess,
+//                                   fail
+//                               );
+//                           },
+//                           fail);
+//                   }
 
-                  // 6
-                  function onCopySuccess(entry) {
-                      $scope.$apply(function() {
-                          var name = entry.nativeURL.substr(entry.nativeURL.lastIndexOf('/') + 1);
-                          $rootScope.imageURL = cordova.file.dataDirectory + name;
-                      });
-                  }
+//                   // 6
+//                   function onCopySuccess(entry) {
+//                       $scope.$apply(function() {
+//                           var name = entry.nativeURL.substr(entry.nativeURL.lastIndexOf('/') + 1);
+//                           $rootScope.imageURL = cordova.file.dataDirectory + name;
+//                       });
+//                   }
 
-                  function fail(error) {
-                      console.log("fail: " + error.code);
-                  }
+//                   function fail(error) {
+//                       console.log("fail: " + error.code);
+//                   }
 
-                  function makeid() {
-                      var text = "";
-                      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//                   function makeid() {
+//                       var text = "";
+//                       var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-                      for (var i = 0; i < 5; i++) {
-                          text += possible.charAt(Math.floor(Math.random() * possible.length));
-                      }
-                      return text;
-                  }
+//                       for (var i = 0; i < 5; i++) {
+//                           text += possible.charAt(Math.floor(Math.random() * possible.length));
+//                       }
+//                       return text;
+//                   }
 
-              }, function(err) {
-                  console.log(err);
-              });
-          };
+//               }, function(err) {
+//                   console.log(err);
+//               });
+//           };
 
-          $scope.urlForImage = function(imageName) {
-              var name = imageName.substr(imageName.lastIndexOf('/') + 1);
-              var trueOrigin = cordova.file.dataDirectory + name;
-              return trueOrigin;
-          };
-      }],
-  };
-})
+//           $scope.urlForImage = function(imageName) {
+//               var name = imageName.substr(imageName.lastIndexOf('/') + 1);
+//               var trueOrigin = cordova.file.dataDirectory + name;
+//               return trueOrigin;
+//           };
+//       }],
+//   };
+// })
 .directive('standardTimeMeridian', function () {
     return {
         restrict: 'AE',
