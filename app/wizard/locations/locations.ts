34cd81cd8d647aeaf2d1cd81ec083ff388bd0f83
Shashank Bharadwaj <shashu10@gmail.com>
2016-06-17 13:02:36 -0700

    +added vscode settings. *fixed ssn bug. *put back old locations

--- app/wizard/locations/locations.ts
+++ app/wizard/locations/locations.ts
@@ -1,40 +1,39 @@
 class LocationsCtrl {
 
-    // private options = [
-    //     'San Francisco',
-    //     'East Bay',
-    //     'South Bay',
-    //     'Peninsula',
-    //     'Other',
-    // ]
-    private groups: any[]
-    private shownState: any
-    private selectedCities: string[] = []
+    private options = [
+        'San Francisco',
+        'East Bay',
+        'South Bay',
+        'Peninsula',
+        'Other',
+    ]
 
-    constructor(public $state: any, public currentUser: CurrentUser, private PostmatesLocations: Object[], public WizardStates: any) {
+    constructor(public $state: any, public currentUser: CurrentUser, private PostmatesLocations: Object[], public WizardStates: any) {}
 
-        // Northern california
-        this.shownState = PostmatesLocations[1]
-    }
 
-    // Toggle for accordion effect
-    toggleState(state) {
-        if (this.isStateShown(state)) this.shownState = null
-        else this.shownState = state
-    }
-    isStateShown(state) {
-        return this.shownState === state
-    }
+    // Accordion //
 
-    // Toggle to save/remove selected cities
-    toggleCity(city) {
-        var i = this.selectedCities.indexOf(city)
-        if (i === -1) this.selectedCities.push(city)
-        else this.selectedCities.splice(i, 1)
-    }
+    // private groups: any[]
+    // private shownState: any
+    // private selectedCities: string[] = []
+
+    // // Toggle for accordion effect
+    // toggleState(state) {
+    //     if (this.isStateShown(state)) this.shownState = null
+    //     else this.shownState = state
+    // }
+    // isStateShown(state) {
+    //     return this.shownState === state
+    // }
+
+    // // Toggle to save/remove selected cities
+    // toggleCity(city) {
+    //     var i = this.selectedCities.indexOf(city)
+    //     if (i === -1) this.selectedCities.push(city)
+    //     else this.selectedCities.splice(i, 1)
+    // }
 
     next() {
-        this.currentUser.locations = this.selectedCities
         this.WizardStates.next()
     }
 
