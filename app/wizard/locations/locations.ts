ef24a19be0818c5bfbd2aa8c62d52652c54a144b
Shashank Bharadwaj <shashu10@gmail.com>
2016-06-27 11:02:57 -0700

    Added other location dropdown

--- app/wizard/locations/locations.ts
+++ app/wizard/locations/locations.ts
@@ -1,15 +1,16 @@
 class LocationsCtrl {
 
-    private options = [
-        'San Francisco',
-        'East Bay',
-        'South Bay',
-        'Peninsula',
-        'Other',
+    private otherCity: string
+    private showOther = false;
+    private options: WGLocation[] = [
+        {selected: false, name: 'san francisco'},
+        {selected: false, name: 'east bay'},
+        {selected: false, name: 'south bay'},
+        {selected: false, name: 'peninsula'},
+        {selected: false, name: 'other'},
     ]
 
     constructor(public $state: ng.ui.IStateService, public currentUser: CurrentUserService, private PostmatesLocations: Object[], public WizardStates: WizardStatesService) {}
-
 
     // Accordion //
 
@@ -26,12 +27,22 @@ class LocationsCtrl {
     //     return this.shownState === state
     // }
 
-    // // Toggle to save/remove selected cities
-    // toggleCity(city) {
-    //     var i = this.selectedCities.indexOf(city)
-    //     if (i === -1) this.selectedCities.push(city)
-    //     else this.selectedCities.splice(i, 1)
-    // }
+    // Toggle to save/remove selected cities
+    tapped(option: WGLocation) {
+        if (option.name === 'other') {
+            this.showOther = option.selected
+            console.log("clicked other");
+        }
+    }
+
+    canContinue(): boolean {
+        // If other is selected, the other name must be at least 2 characters long
+        const other = _.find(this.options, (o: WGLocation) => o.name === 'other')
+        if (other.selected) return (this.otherCity || "").length >= 2
+
+        // At least one options must be selected
+        return _.reduce(this.options, (result: boolean, value: WGLocation, key) => (result || value.selected), false);
+    }
 
     next() {
         this.WizardStates.next()
@@ -39,4 +50,8 @@ class LocationsCtrl {
 
 }
 
+interface WGLocation {
+    selected: boolean
+    name: string
+}
 LocationsCtrl.$inject = ["$state", "currentUser", "PostmatesLocations", "WizardStates"]
