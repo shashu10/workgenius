60cbadd6c4a981c6361df47642d55eb00823fb54
Oskar Zhang <i@oskarzhang.me>
2016-07-18 14:59:06 -0700

    tutorial page

---
+++ app/tutorial/tutorial.ts
@@ -0,0 +1,20 @@
+
+class WGIntroCtrl {
+	constructor(public $state:angular.ui.IStateService) {
+		
+	}
+	startApp()  {
+		this.$state.go("welcome");
+	}
+	next() {
+
+	}
+
+}
+
+WGIntroCtrl.$inject = ["$state"];
+
+angular.module('wg.tutorial',[])
+
+.controller('TutorialCtrl', WGIntroCtrl);
+
