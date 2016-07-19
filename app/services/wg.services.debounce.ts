b6f3fb579af869de829a8aff3142edd9b82d0c00
Shashank Bharadwaj <shashu10@gmail.com>
2016-07-18 17:46:53 -0700

    goal working with deferred

--- app/services/wg.services.debounce.ts
+++ app/services/wg.services.debounce.ts
@@ -2,27 +2,28 @@ class WGDebounce {
     
     constructor(public $timeout: angular.ITimeoutService, public $q: angular.IQService) {}
 
-    create(func, wait = 500, immediate = false) {
+    create(func, wait = 300, immediate = false) {
         var timeout
         var deferred = this.$q.defer()
-        return () => {
-            var context = this,
-                // args = arguments
+        var that = this;
+        return function () {
+            var context = that,
+                args = arguments
             var later = () => {
                 timeout = null
                 if (!immediate) {
-                    deferred.resolve(func.apply(context))
-                    deferred = this.$q.defer()
+                    deferred.resolve(func.apply(context, args))
+                    deferred = that.$q.defer()
                 }
             }
             var callNow = immediate && !timeout
             if (timeout) {
-                this.$timeout.cancel(timeout)
+                that.$timeout.cancel(timeout)
             }
-            timeout = this.$timeout(later, wait)
+            timeout = that.$timeout(later, wait)
             if (callNow) {
-                deferred.resolve(func.apply(context))
-                deferred = this.$q.defer()
+                deferred.resolve(func.apply(context, args))
+                deferred = that.$q.defer()
             }
             return deferred.promise
         }
